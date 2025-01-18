c881c6f94b779c2ab736ab361aaa07280a19840b
SuhanaShakir7 <busheskhanbushes3271@gmail.com>
2025-01-18 12:42:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+
+// // 1. Declare 3 variables in one statement. 
+// let a=12;
+// let b=14;
+//  let c=12;
+
+
+// // 2. Declare 5 legal & 5 illegal variable names. 
+// // let  one =var123
+// // let  two= @usernamee 
+// // let four=var
+// // let five=Side..
+
+
+
+
+// let g=145;
+// let d=Var123;
+// let e=isCute;
+// let f=Agirlis;
+
+
+// // 3. Display this in your browser 
+// document.write('Rules for naming JS variables” ');
+// document.write('Variable names can only contain camelCase, underscore, dollarsign, letter and number.' )
+// document.write('Variable names should not be JS keyword  ')
+// document.write(') Variable names are case sensitive');
+
+
+
+// // chapter4
+
+
+// // 1. Declare a variable called age & assign to it your age. Show your age in an alert box. 
+// let age=18;
+// alert("i am "+ age +" year old")
+
+
+
+// // // 2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page. 
+// // Show his/her number of visits on your web page. For example: “You have visited this site N times”. 
+
+
+
+// let count=localStorage.getItem('visitCount')|| 0
+// ;
+// count++;localStorage.setItem('visitCount',count);
+// document.body.innerHTML=`you have visit this site ${count} times`;
+
+
+
+
+
+
+
+
+// // 3. Declare a variable called birthYear & assign to it your birth year.
+
+
+// let birthYear=2007;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // Show the following message in your browser: 
+// // let store="cloths"
+// // let  visitorsName='jone';
+// // let productTitle=' T-shirt(s)'
+// // let quantity='5';
+
+// // document.body.innerHTML=`${visitorsName} ordered ${quantity}  ${productTitle}  on XYZ Clothing store`;
+// //  // Correct variable initialization
+ 
